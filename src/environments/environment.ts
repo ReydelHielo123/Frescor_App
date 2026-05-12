1b835177ad327fa5e979a54721cf514b1ba86709
Diego Romero <diegitoiacc19992004@gmail.com>
2026-05-12 02:28:43 -0300

    Actualizar URL API produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:7175/api'
+    production: true,
+    apiUrl: 'https://frescorapi-production.up.railway.app/api'
 };
